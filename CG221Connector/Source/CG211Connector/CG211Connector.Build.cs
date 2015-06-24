498372dca5646763d0f4e65cf03de90b5d6e5afb
tsky <thomas.kollakowsky@w-hs.de>
2015-06-24 11:15:19 +0200

    rename dir

---
+++ CG221Connector/Source/CG211Connector/CG211Connector.Build.cs
@@ -0,0 +1,69 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class CG211Connector : ModuleRules
+{
+	public CG211Connector(TargetInfo Target)
+	{
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"CG211Connector/Public"
+
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"CG211Connector/Private",
+				
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"Engine",
+                "InputCore",
+                "Slate",
+                "SlateCore",
+                "EditorStyle",
+                "CoreUObject",
+				"Sockets",
+				"Networking",
+				"RHI",
+				"RenderCore",
+				"Renderer",
+				"ShaderCore",
+				"HeadMountedDisplay",
+				"Slate",
+				"SlateCore",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Slate", "SlateCore"
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ CG221Connector/Source/CG211Connector/Public/ICG211ConnectorPlugin.h
@@ -0,0 +1,59 @@
+/**
+	Copyright (c) 2015, tsky
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice, this
+	  list of conditions and the following disclaimer.
+
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+**/
+
+#pragma once
+
+#include "ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(CG221ConnectorLog, Log, All);
+
+class ICG211ConnectorPlugin : public IModuleInterface
+{
+public:
+
+	/**
+	 * Singleton-like access to this module's interface.  This is just for convenience!
+	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+	 *
+	 * @return Returns singleton instance, loading the module on demand if needed
+	 */
+	static inline ICG211ConnectorPlugin& Get()
+	{
+		return FModuleManager::LoadModuleChecked<ICG211ConnectorPlugin>("CG211ConnectorPlugin");
+	}
+
+	/**
+	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+	 *
+	 * @return True if the module is loaded and ready to use
+	 */
+	static inline bool IsAvailable()
+	{
+		return FModuleManager::Get().IsModuleLoaded( "CG211ConnectorPlugin" );
+	}
+
+};
